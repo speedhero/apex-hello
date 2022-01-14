aa0748ac619807cd624bddc40a4d72691167d0ca
蒋涛(银行业务事业部技术部银行项目实施部) <jiangtao@apexsoft.com.cn>
2022-01-14 10:57:57 +0800

    server模块变更

--- src/main/java/apexsoft/HelloWorldApplication.java
+++ src/main/java/apexsoft/HelloWorldApplication.java
@@ -1,11 +1,11 @@
 package apexsoft;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
 
 /**
  * @author jiangtao
